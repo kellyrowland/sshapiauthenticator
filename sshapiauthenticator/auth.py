6a3a209f002572bf89f7839202a15d5438dfdecf
Kelly Rowland <kellylynnerowland@gmail.com>
2018-11-06 12:19:31 -0800

    spacing

--- sshapiauthenticator/auth.py
+++ sshapiauthenticator/auth.py
@@ -84,10 +84,10 @@ class SSHAPIAuthenticator(Authenticator):
             remote_host = 'cori19.nersc.gov'
             async with asyncssh.connect(remote_host,username=username,known_hosts=None) as conn:
                 result = await conn.run("myquota -c")
-                if result.exit_status:
-                    e = web.HTTPError(507,reason="Insufficient Storage")
-                    e.my_message = "There is insufficient space in your home directory; please clear up some files and try again."
-                    raise e
-                    return None
-                else:
-                    return username
+            if result.exit_status:
+                e = web.HTTPError(507,reason="Insufficient Storage")
+                e.my_message = "There is insufficient space in your home directory; please clear up some files and try again."
+                raise e
+                return None
+            else:
+                return username
