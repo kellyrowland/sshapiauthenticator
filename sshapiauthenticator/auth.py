acd10e214aa5e167a9f5fa9efd02d2eeebc96541
Kelly Rowland <kellylynnerowland@gmail.com>
2018-11-06 15:04:48 -0800

    try direct login

--- sshapiauthenticator/auth.py
+++ sshapiauthenticator/auth.py
@@ -81,7 +81,7 @@ class SSHAPIAuthenticator(Authenticator):
                 self.log.warning("SSH Auth API Authentication failed: ")
             return None
         else:
-            command = "ssh {user}@cori19.nersc.gov 'myquota -c'".format(user=username)
+            command = "ssh {user}@cori01-224.nersc.gov 'myquota -c'".format(user=username)
             command = shlex.split(command)
             result = call(command)
             if result:
